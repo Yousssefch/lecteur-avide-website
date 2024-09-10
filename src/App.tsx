06761da6a7c7454de050b585b2bd0d51149d7d9b
Yousssefch <youssefchakir003@gmail.com>
2024-09-10 16:09:53 -0400

    Changed basename

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 function App() {  
   return (
     <>
-    <BrowserRouter basename='lecteur-avide-website/'>
+    <BrowserRouter basename='/lecteur-avide-website/'>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/Events" element={<Eventpage />} />
