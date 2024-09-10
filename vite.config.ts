50d1d40600201cc1f33a713aba7d34610939cf34
Yousssefch <youssefchakir003@gmail.com>
2024-09-09 23:01:53 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
 
 })
-
-build: {
-  chunkSizeWarningLimit: 2000
-}
