9159034ca66ac114d3e0dc69fc247b31e5c72834
Youssef chakir <youssefchakir003@gmail.com>
2024-10-10 01:05:07 -0400

    Configs

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '',
+  base: '/lecteur-avide-website/',
   build: {
     chunkSizeWarningLimit: 2000,
   },
