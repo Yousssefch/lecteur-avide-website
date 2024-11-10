21345665fcbf4fdb959ff70fa3d993525b1e1d39
Youssef chakir <youssefchakir003@gmail.com>
2024-11-09 22:20:06 -0500

    added a join us button

--- src/Components/Global/Navbar.tsx
+++ src/Components/Global/Navbar.tsx
@@ -34,6 +34,7 @@ function Navbar() {
         <li className="navLi hideOnMobile"><a href={basename +"#/Books"} className="NavLink">Books</a></li>
         <li className="navLi hideOnMobile"><a href={basename + "#/Events"} className="NavLink">Events</a></li>
         <li className="navLi hideOnMobile"><a href={basename + "#/Contact"} className="NavLink">Contact Us</a></li>
+        <button onClick={() => location.href = "https://share.hsforms.com/1G-pWGQAOSIS9cgIjKiM5gwnl5y9"} className="navLi JoinButton">Join Us!</button>
         <li className="navLi" onClick= {() => {showSideBar()}}><a  className="NavLink hideOnLargerScreen" ><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z"/></svg></a></li>
       </ul>
 
